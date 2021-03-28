9cd7b18a3abaa2ed54ffba3d92bed5fcade0d47d
Mad Wiki <wudifengxiang@gmail.com>
2021-03-29 07:39:02 +0800

    fix build.sh, update near-sdk to latest

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cargo build --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/cdot_token.wasm ./out/main.wasm
+cp target/wasm32-unknown-unknown/release/oct_token.wasm ./out/main.wasm
